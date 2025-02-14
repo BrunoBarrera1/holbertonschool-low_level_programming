a250f7b3f86f70f8e490f53b507b954a46fdfa41
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-02-14 18:10:46 +0000

    5-print.c

---
+++ hello_world/5-print.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints a message on screen
+ * Description: prints the message using printf
+ * Return: returns 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
