2d785431e2fa43571aeb0674168db69970a27030
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-02-14 18:40:46 +0000

    5-printf.

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 /**
  * main - prints a message on screen
  * Description: prints the message using printf
- * return: returns 0
+ * Return: returns 0
  */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
