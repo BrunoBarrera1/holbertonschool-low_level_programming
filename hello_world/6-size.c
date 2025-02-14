4c8e27dc4631f5ffef37111ba305e2179d4a9416
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-02-14 19:11:48 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints a text on screen
+ * Description: prints message using printf
+ * Return: returns 0
+ */
+
+int main(void)
+{
+printf("size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of a char: %zu byte(s)\n", sizeof(int));
+printf("size of a char: %zu byte(s)\n", sizeof(long));
+printf("size of a char: %zu byte(s)\n", sizeof(long long));
+printf("size of a char: %zu byte(s)\n", sizeof(float));
+return (0);
+}
