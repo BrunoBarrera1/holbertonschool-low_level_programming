a2b981210ec2edabf4b6e1108a18bcf5d54971cb
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-02-14 19:25:04 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 printf("size of a char: 1 byte(s)\n", sizeof(char));
-printf("size of a int: 4 byte(s)\n", sizeof(int));
+printf("size of an int: 4 byte(s)\n", sizeof(int));
 printf("size of a long: 8 byte(s)\n", sizeof(long));
 printf("size of a long long: 8 byte(s)\n", sizeof(long long));
 printf("size of a float: 4 byte(s)\n", sizeof(float));
