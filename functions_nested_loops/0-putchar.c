3dd9d6cecaa428d3e919ec51c917cc9bb3953035
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-02-19 13:21:03 +0000

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints printf function
+ *
+ * Return: always returns 0 (Success)
+ */
+int main(void)
+{
+putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
