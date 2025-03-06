d7bf29bef7d625a4bcbba34ae3baf453eceb26e0
BrunoBarrera1 <brunobarrera222.180@gmail.com>
2025-03-06 13:53:07 +0000

    nargs

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * @argc: num of command line arguments
- * @argv: array of command line arguments
- * Return: 0 (Success)
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array of command line arguments.
+ *
+ * Return: 0 on (Success)
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("Num of arguments: %d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
