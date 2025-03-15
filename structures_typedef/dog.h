6c67bbe59ff16dc0a9730909858463a3d30e7824
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-03-15 21:59:39 +0000

    ive got that dawg in me

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,13 @@
-struct dog {
+#include <stdio.h>
+
+/**
+ * struct dog - structure representating a dawg
+ * @name: pointer to da dawgs name (string)
+ * @age: age of that dawg (float)
+ * @owner: pointer to owners name (string)
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
